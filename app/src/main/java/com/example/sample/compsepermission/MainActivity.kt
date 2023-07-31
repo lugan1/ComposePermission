9274050cbbc7aea4362e264e043ec9c3ef6c3b46
이후기 <lugan@naver.com>
2023-08-01 08:33:27 +0900

    * 권한 요청 샘플 코드 작성

--- app/src/main/java/com/example/sample/compsepermission/MainActivity.kt
+++ app/src/main/java/com/example/sample/compsepermission/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.sample.compsepermission
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -11,20 +15,20 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.sample.compsepermission.ui.theme.CompsePermissionTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -39,6 +43,12 @@ class MainActivity : ComponentActivity() {
 fun MainScreen() {
     val viewModel = viewModel<MainViewModel>()
     val dialogQueue = viewModel.visiblePermissionDialogQueue
+
+    val permissionToRequest = arrayOf(
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.RECORD_AUDIO,
+        android.Manifest.permission.CALL_PHONE
+    )
     
     val cameraPermissionResultLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission(),
@@ -47,6 +57,18 @@ fun MainScreen() {
                 permission = "CAMERA",
                 isGranted = isGranted
             )
+        }
+    )
+
+    val multiplePermissionResultLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            permissionToRequest.forEach { permission ->
+                viewModel.onPermissionResult(
+                    permission = permission,
+                    isGranted = permissions[permission] ?: false
+                )
+            }
         }
     )
 
@@ -62,26 +84,59 @@ fun MainScreen() {
             Text(text = "권한 요청")
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            multiplePermissionResultLauncher.launch(permissionToRequest)
+        }) {
             Text(text = "여러개의 권한 요청")
+        }
+
+        val activity = LocalContext.current as ComponentActivity
+
+        dialogQueue
+            .reversed()
+            .forEach { permission ->
+            PermissionDialog(
+                permissionTextProvider = when(permission) {
+                    android.Manifest.permission.CAMERA -> CameraPermissionTextProvider()
+                    android.Manifest.permission.RECORD_AUDIO -> RecordAudioPermissionTextProvider()
+                    android.Manifest.permission.CALL_PHONE -> PhoneCallPermissionTextProvider()
+                    else -> return@forEach
+                },
+                isPermanentlyDeclined = !shouldShowRequestPermissionRationale(activity, permission),
+                onDismiss = viewModel::dismissDialog,
+                onConfirm = {
+                    viewModel.dismissDialog()
+                    multiplePermissionResultLauncher.launch(
+                        arrayOf(permission)
+                    )
+                },
+                onGoToAppSettingsClick = { activity.openAppSettings() }
+            )
         }
     }
 }
 
 class MainViewModel: ViewModel() {
-    // [RECORD_AUDIO, CAMERA]
+    // [RECORD_AUDIO, CALL_PHONE, CAMERA]
     val visiblePermissionDialogQueue = mutableStateListOf<String>()
 
     fun dismissDialog() {
-        visiblePermissionDialogQueue.removeLast()
+        visiblePermissionDialogQueue.removeFirst()
     }
 
     fun onPermissionResult(
         permission: String,
         isGranted: Boolean
     ) {
-        if(!isGranted) {
+        if(!isGranted && !visiblePermissionDialogQueue.contains(permission)) {
             visiblePermissionDialogQueue.add(permission)
         }
     }
+}
+
+fun Activity.openAppSettings() {
+    Intent(
+        Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+        Uri.fromParts("package", packageName, null)
+    ).also(::startActivity)
 }
--- app/src/main/java/com/example/sample/compsepermission/PermissionDialog.kt
+++ app/src/main/java/com/example/sample/compsepermission/PermissionDialog.kt
@@ -1,12 +1,95 @@
 package com.example.sample.compsepermission
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun PermissionDialog() {
+fun PermissionDialog(
+    permissionTextProvider: PermissionTextProvider,
+    isPermanentlyDeclined: Boolean,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    onGoToAppSettingsClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
     AlertDialog(
-        onDismissRequest = { /*TODO*/ },
-        confirmButton = { /*TODO*/ }
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            Column(modifier.height(70.dp)) {
+                Divider()
+                Text(
+                    text = if(isPermanentlyDeclined) {
+                        "권한이 필요합니다. 설정에서 권한을 허용해주세요."
+                    } else {
+                        "권한이 필요합니다. 권한을 허용해주세요."
+                    },
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clickable {
+                            if (isPermanentlyDeclined) {
+                                onGoToAppSettingsClick()
+                            } else {
+                                onConfirm()
+                            }
+                        }
+                        .padding(16.dp)
+                    )
+            }
+        },
+        title = {
+                Text(text = "권한이 필요합니다.")
+        },
+        text = {
+               Text(text = permissionTextProvider.getDescription(isPermanentlyDeclined))
+        },
+        modifier = modifier
     )
+}
+
+interface PermissionTextProvider {
+    fun getDescription(isPermanentlyDeclined: Boolean): String
+}
+
+class CameraPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "당신은 카메라 권한을 영구적으로 거부한 것 같습니다. 앱 설정으로 이동하여 권한을 허용할 수 있습니다."
+        } else {
+            "이 앱은 친구들이 영상통화에서 당신을 볼 수 있도록 카메라에 대한 접근 권한이 필요합니다."
+        }
+    }
+}
+
+class RecordAudioPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "당신은 음성마이크 권한을 영구적으로 거부한 것 같습니다. 앱 설정으로 이동하여 권한을 허용할 수 있습니다."
+        } else {
+            "이 앱은 친구들이 영상통화에서 당신의 음성을 들을 수 있도록 음성마이크에 대한 접근 권한이 필요합니다."
+        }
+    }
+}
+
+class PhoneCallPermissionTextProvider : PermissionTextProvider {
+    override fun getDescription(isPermanentlyDeclined: Boolean): String {
+        return if(isPermanentlyDeclined) {
+            "전화 걸기 권한을 영구적으로 거부한 것 같습니다. 앱 설정으로 이동하여 권한을 허용할 수 있습니다."
+        } else {
+            "이 앱은 친구들과 대화할 수 있도록 전화 걸기 권한이 필요합니다."
+        }
+    }
+
 }
